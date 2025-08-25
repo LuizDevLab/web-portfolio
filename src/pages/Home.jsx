617b22ca45e07e6efb99c651a9004cbb771b254c
Luiz Henrique <luizgthub@gmail.com>
2025-08-25 16:00:27 -0300

    feat: add the navbar component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { ThemeToggle } from "../components/ThemeToggle"
 import { StarBackground } from '@/components/StarBackground'
+import { NavBar } from "../components/NavBar"
 
 export const Home = () => {
 
@@ -11,6 +12,7 @@ export const Home = () => {
     <StarBackground />
 
     {/* Navbar */}
+    <NavBar />
 
     {/* main content */}
 
